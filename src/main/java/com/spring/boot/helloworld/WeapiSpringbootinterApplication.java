18f8edfd40a7f6f2b6637ad8006ecd02e7fb56ad
857796028 <857796028@qq.com>
2018-11-13 10:26:51 +0800

    001.第1次提交

---
+++ src/main/java/com/spring/boot/helloworld/WeapiSpringbootinterApplication.java
@@ -0,0 +1,12 @@
+package com.spring.boot.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeapiSpringbootinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeapiSpringbootinterApplication.class, args);
+	}
+}
